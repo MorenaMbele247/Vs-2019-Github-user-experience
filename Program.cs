e5a25d45591948bb681cc677c45891cdaafe22c2
Lesedi <morenambele247@gmail.com>
2021-10-04 20:20:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vs_2019_Github_user_experience
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
